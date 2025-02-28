42b1450336e7c3f8cbf5d8c1b02d6d0b1d7393f6
sahrudin11 <sahrudinu48@gmail.com>
2025-02-28 21:25:17 +0700

    setup project config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,29 @@
 import type { Config } from "tailwindcss";
 
+const colors = {
+  color1: "#5C2D2D",
+  color2: "#FF48B6",
+  color3: "#FFA740",
+  color4: "#07B704",
+  dark1: "#000000",
+  light1: "#ffffff",
+  light2: "#F7F7F9",
+  light3: "#E4E6EB",
+};
+
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        ...colors,
+      },
+      fill: { ...colors },
+      fontFamily: {
+        sans: ["'Poppins', sans-serif"],
       },
     },
   },
